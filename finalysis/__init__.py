b53a327d03764492180ff8011ff6906ca94d9e29
sarang-uob <sb4056@bath.ac.uk>
2025-10-17 23:04:03 +0100

    Enhance visualisation module by adding multiple plotting functions and improving heatmap representation
    - Updated __init__.py files to include new plotting functions: plot_top_pairs_heatmap, plot_top_pairs_scatter, plot_spread_zscore, plot_cumulative_returns, and plot_all.
    - Refactored plot_top_pairs_heatmap function to use matplotlib for rendering the heatmap instead of seaborn, providing better control over aesthetics and annotations.
    - Added colorbar and improved text annotations for correlation values in the heatmap for better readability.

--- finalysis/__init__.py
+++ finalysis/__init__.py
@@ -2,9 +2,9 @@
 from .data_loader.data_loader import load_data
 from .metrics.basic_info import describe_dataset
 from .metrics.stock_pairs_trading_analysis import run_analysis
-from .visualisation.graphs import plot_price_series
+from .visualisation.graphs import plot_price_series, plot_top_pairs_heatmap,plot_top_pairs_scatter,plot_spread_zscore,plot_cumulative_returns,plot_top_pairs_heatmap,plot_all
 
 
-__all__ = ['load_data', 'describe_dataset', 'run_analysis',"plot_price_series"]
+__all__ = ['load_data', 'describe_dataset', 'run_analysis',"plot_price_series","plot_top_pairs_heatmap","plot_top_pairs_scatter","plot_spread_zscore","plot_cumulative_returns","plot_top_pairs_heatmap","plot_all",]
 
 
